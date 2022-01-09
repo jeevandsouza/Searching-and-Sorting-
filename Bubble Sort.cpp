0e5495c9f118d0799fed5be40c0f251408801748
jeevandsouza <jeevanjd97@gmail.com>
2022-01-09 17:01:26 -0600

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -13,10 +13,16 @@ void BubbleSort(int array[],int arraySize)
 {
 	for(int i=0;i<arraySize-1;i++)
 	{
+			bool swapped;
 		for(int j=0;j<arraySize-i-1;j++)
 		{
 			if(array[j] > array[j+1])
-			Swap(&array[j],&array[j+1]);
+			{
+				Swap(&array[j],&array[j+1]);
+				swapped = true;
+			}
+				if(!swapped)
+		                   break;
 		}
 	}
 }
